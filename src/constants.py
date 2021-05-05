8d9c068ca4f5eab6ec05c8c4f47c72d4e61350f9
ryoma116 <uma19880116>
2021-05-06 06:00:59 +0900

    [#25] フォローされているユーザを除外する

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,6 @@ from enum import Enum
 WEEKDAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 JA_WEEKDAYS = ["月", "火", "水", "木", "金", "土", "日"]
 
-MAX_SEARCH_COUNT = 100
 FULL_TEXT_TWEET_MODE = "extended"
 
 # 当日のツイートを除外したい場合はTrueとする（時間帯分析時は24時間分取得できない当日は除外した方がよさそう）
@@ -14,3 +13,15 @@ class FfRatioOrderModes(Enum):
     HIGH = "high"
     LOW = "low"
     CLOSE_TO_ONE = "close_to_one"
+
+
+SEARCH_API_PATH = "/search/tweets"
+FOLLOWER_IDS_API_PATH = "/followers/ids"
+API_TYPES = {
+    SEARCH_API_PATH: "search",
+    FOLLOWER_IDS_API_PATH: "followers",
+}
+API_COUNTS = {
+    SEARCH_API_PATH: 100,
+    FOLLOWER_IDS_API_PATH: 5000,
+}
--- src/errors.py
+++ src/errors.py
@@ -8,3 +8,15 @@ class ParameterError(Exception):
     """パラメータ指定の不正を知らせる例外クラス"""
 
     pass
+
+
+class RateLimitError(Exception):
+    """リクエスト上限到達を知らせる例外クラス"""
+
+    pass
+
+
+class TwitterApiError(Exception):
+    """TwitterAPIからの応答エラーを知らせる例外クラス"""
+
+    pass
