b18edc1941f81459b256af2c6e9baabeec6908ef
ryoma116 <uma19880116>
2021-05-07 05:26:35 +0900

    [#27]FF比が1に近いユーザランキング取得APIは別で切り出す、他
    - FF比が1に近いユーザランキング取得APIは別で切り出す(#27)
    - フォロワー、フォロー中の値を別でセットできるよう修正（自分以外の分析もできるように）
    - ログ出力をprintからloggingに変更(#32)

--- src/api.py
+++ src/api.py
@@ -1,15 +1,18 @@
+import logging
+
 import pandas
 import pytz
 import tweepy
 
-from .constants import FfRatioOrderModes
-from .errors import ParameterError
 from .filters import filter_user
 from .graphs import make_daily_tweet_users_graph, make_daily_tweets_graph
 from .rankings import make_user_ranking
 from .tweets import search_tweets
-from .users import get_follower_ids
+from .users import get_follower_ids, get_following_ids
 from .validates import validate_tweet_exists
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class TwiVisuAPI:
@@ -25,18 +28,38 @@ class TwiVisuAPI:
         self._timezone = pytz.timezone(timezone)
 
     def search_tweets(self, search_word: str, advanced_query: str, limit: int = None):
+        logger.info("-- search_tweets Start")
         search_query = search_word + " " + advanced_query
-        follower_ids = get_follower_ids(api=self._api, user_id=self._api.me().id)
         tweets = search_tweets(
             api=self._api,
             search_query=search_query,
             limit=limit,
             timezone=self._timezone,
-            follower_ids=follower_ids,
         )
         self._search_word = search_word
         self._search_query = search_query
         self._df = pandas.DataFrame(tweets)
+        logger.info(f"-- search_tweets End（合計{'{:,}'.format(len(tweets))}）")
+
+    def set_followers(self, user_screen_name):
+        logger.info("-- set_followers Start")
+        follower_ids = get_follower_ids(
+            api=self._api, user_screen_name=user_screen_name
+        )
+        self._df["follower"] = self._df.apply(
+            lambda x: x.user_id in follower_ids, axis=1
+        )
+        logger.info(f"-- set_followers End（合計{'{:,}'.format(len(follower_ids))}）")
+
+    def set_following(self, user_screen_name):
+        logger.info("-- set_following Start")
+        following_ids = get_following_ids(
+            api=self._api, user_screen_name=user_screen_name
+        )
+        self._df["following"] = self._df.apply(
+            lambda x: x.user_id in following_ids, axis=1
+        )
+        logger.info(f"-- set_following End（合計{'{:,}'.format(len(following_ids))}）")
 
     def make_daily_tweets_graph(self, **kwargs):
         validate_tweet_exists(self._df)
@@ -60,7 +83,7 @@ class TwiVisuAPI:
             search_query=self._search_query,
             col="tweets_count",
             ascending=False,
-            **kwargs
+            **kwargs,
         )
 
     def make_followers_user_ranking(self, **kwargs):
@@ -71,7 +94,7 @@ class TwiVisuAPI:
             search_query=self._search_query,
             col="followers_count",
             ascending=False,
-            **kwargs
+            **kwargs,
         )
 
     def make_friends_user_ranking(self, **kwargs):
@@ -82,36 +105,28 @@ class TwiVisuAPI:
             search_query=self._search_query,
             col="friends_count",
             ascending=False,
-            **kwargs
+            **kwargs,
         )
 
-    def make_ff_ratio_user_ranking(
-        self, order_mode: str = FfRatioOrderModes.HIGH.value, **kwargs
-    ):
+    def make_ff_ratio_user_ranking(self, **kwargs):
         validate_tweet_exists(self._df)
-
-        col = "ff_ratio"
-        if order_mode == FfRatioOrderModes.HIGH.value:
-            ascending = False
-
-        elif order_mode == FfRatioOrderModes.LOW.value:
-            ascending = True
-
-        elif order_mode == FfRatioOrderModes.CLOSE_TO_ONE.value:
-            col = "ff_ratio_close_to_one"
-            ascending = True
-
-        else:
-            raise ParameterError(
-                "Please select a valid choice for order_mode. (high, low, close_to_one)"
-            )
-
         make_user_ranking(
             self._df,
             search_word=self._search_word,
             search_query=self._search_query,
-            col=col,
-            ascending=ascending,
+            col="ff_ratio",
+            value_fmt="{:.2f}",
+            **kwargs,
+        )
+
+    def make_ff_ratio_close_to_one_user_ranking(self, **kwargs):
+        validate_tweet_exists(self._df)
+        make_user_ranking(
+            self._df,
+            search_word=self._search_word,
+            search_query=self._search_query,
+            col="ff_ratio_close_to_one",
+            ascending=True,
             value_fmt="{:.4f}",
-            **kwargs
+            **kwargs,
         )
